5fc1c3f986b1f999bc6b1bc9563b2650f88affce
javicastejon <145163627+javicastejon@users.noreply.github.com>
2024-10-08 21:36:22 +0200

    Upload solved activities

--- Actividad1_2/Persona.java
+++ Actividad1_2/Persona.java
@@ -1,38 +1,25 @@
-/*
- * Esta clase Persona tiene dos campos (nombre y edad), 
- * un constructor que recibe nombre y edad, y métodos para establecer y 
- * obtener estos valores. Además, implementa la interfaz Serializable,
- * lo que permite que los objetos de esta clase se serialicen y deserialicen.
- */
-import java.io.Serializable;
+package ficheros.act02;
 
-public class Persona implements Serializable {
+import java.io.Serializable;
+public class Persona implements Serializable{
 	private String nombre;
 	private int edad;
-
-	public Persona(String nombre, int edad) {
+	
+	public Persona(String nombre, int edad){
 		this.nombre = nombre;
 		this.edad = edad;
 	}
-
-	public Persona() {
+	
+	public Persona(){
 		this.nombre = null;
 	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public String getNombre() {
-		return this.nombre;
-	}
-
-	public int getEdad() {
-		return this.edad;
-	}
-
+    
+	public void setNombre(String nombre){this.nombre = nombre;}
+	public void setEdad(int edad){this.edad = edad;}
+	
+	public String getNombre() {return this.nombre;}
+	public int getEdad() {return this.edad;}
+	
 }
+
+
